c08945332e348cd002d17862a4f5c2cc81e01a69
corazon714 <mert.kaya.98@protonmail.com>
2024-01-14 23:27:01 +0300

    backup

--- 2/moving_obstacle_down_up.gd
+++ 2/moving_obstacle_down_up.gd
@@ -7,7 +7,7 @@ func useUpDownAnim():
 	var tween = create_tween()
 	tween.set_loops()
 	tween.set_trans(Tween.TRANS_SINE)
-	tween.tween_property(self, "global_position", global_position + destination, duration)
+	tween.tween_property(self, "global_position", global_position - destination, duration)
 	tween.tween_property(self, "global_position", global_position, duration)
 
 # Called when the node enters the scene tree for the first time.
